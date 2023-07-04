0134862096bd991591bf7ba3c5b12b7f354c41bc
Jasmine Kwong <jasminekkwong@gmail.com>
2023-07-04 00:23:25 -0600

    about me

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <MainMenu />
+  <AboutMe />
 </template>
 
 <script setup lang="ts">
 import MainMenu from '@/components/MainMenu.vue'
 import '@/main.css'
+import AboutMe from '@/components/AboutMe.vue';
 </script>
 
 <style scoped></style>
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="about-me">
+    <h1 id="about-me-header">hi, i'm jasmine :)</h1>
+    <br />
+
+    <p id="about-me-description">
+      I'm from Calgary, AB and graduated from UBC in 2021. In my free time, I like to work on small
+      projects, like this site, and learn new things. Currently, I'm trying to read about
+      distributed systems and event driven architecture. Outside of computers, I like making
+      stickers, throwing pottery, and reading.
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#about-me {
+  margin-top: 100px;
+  margin-left: 10%;
+  width: 50%;
+}
+
+#about-me-header {
+  animation: 1s ease-in 0s slideUpHeader;
+}
+
+#about-me-description {
+  line-height: 2em;
+  animation: 2s ease-in 0s slideUpDescription;
+}
+
+@keyframes slideUpHeader {
+  from {
+    transform: translateY(20px);
+    opacity: 0;
+  }
+}
+
+@keyframes slideUpDescription {
+  0% {
+    transform: translateY(20px);
+    opacity: 0;
+  }
+
+  50% {
+    transform: translateY(10%);
+    opacity: 0;
+  }
+}
+</style>
